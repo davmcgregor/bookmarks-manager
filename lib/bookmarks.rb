f7788200015e183cffc9f0a703ad754a7fe29ddf
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-10 17:40:38 +0000

    Attach database to Ruby app

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,5 +1,9 @@
+require 'pg'
+
 class Bookmarks
   def self.all
-    ["google.com", "wikipedia.com", "github.com"]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end 
 end 
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -3,7 +3,7 @@ require 'bookmarks'
 describe Bookmarks do 
   describe "#all" do 
     it 'should show all bookmarks' do 
-      expect(Bookmarks.all).to be_an Array
+      expect(Bookmarks.all).to include("http://www.makersacademy.com")
     end 
   end 
 end   
